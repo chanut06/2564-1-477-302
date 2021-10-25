a23d308f15f46baaeb00ba18561c5f2404c6e290
chanut06 <sunna.bi89@gmail.com>
2021-10-25 11:58:30 +0700

    PreProjectnaka

---
+++ ProjectPre/moneymarry.php
@@ -0,0 +1,88 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    </style>
+     <link rel="stylesheet" href="Prestyle.css" type="text/css">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Pridi:wght@300&display=swap" rel="stylesheet">
+    
+    <script>
+
+    </script>
+    <title>I like ...I share</title>
+</head>
+<body>
+    <div class="header">
+        <a href="#default" class="logo">I like ...I share</a>
+        <div class="header-right">
+            <a class="active" href="#home">หน้าหลัก</a>
+            <a href="#news">แนะนำเรื่องในเทรนด์</a>
+            <a href="#about">เริ่มสร้างบทความ</a>
+            <a href="#contact">ติดต่อ</a>
+    <!-- Load icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- The form -->
+    <form class="example" action="action_page.php">
+        <input type="text" placeholder="Search.." name="search">
+        <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="leftcolumn">
+          <div class="card">
+            <h2>‘สินสอด’ยังควรมีอยู่อีกไหมในยุคนี้?  </h2>
+            <img src="image/money1.jpg" style="height:200px;">
+            <p>สืบเนื่องจากกระแสสังคม จากประเด็นการแสดงความคิดเห็นต่อประเด็น “สินสอด” ของชายหนุ่มคนหนึ่งที่ออกมาโพสต์ลงสื่อออนไลน์ และยิ่งทำเอาร้อนระอุไปทั่วโลกออนไลน์ เมื่อนักแสดงสาว “ก้อย – อรัชพร โภคินภากร” ออกมาแสดงความคิดเห็นต่อประเด็นดังกล่าวผ่านไอจีสตอรี่ว่า
+              ‘ส่วนตัวคิดว่าควรยกเลิกระบบสินสอดค่ะ แนวคิดการวัดค่าผู้หญิงด้วยเงินจะได้หมดไปสักที น่ารำคาญค่ะ’
+              ทำให้สังคมกลับมาพูดถึงเป็นอย่างมาก โดยผู้หญิงส่วนใหญ่ในโลกโซเชี่ยลบอว่า “การวัดค่าผู้หญิงด้วยเงินควรหมดไปได้แล้ว” เพราะสินสอด พันธนาการ ผู้หญิง
+              ในมุมมองของผู้อ่านมีความคิดเห็นว่าสังคมปัจจุบันสมควรที่จะยกเลิกสินสอดหรือไม่ จึงขอเชิญชวนผู้อ่านที่เห็นด้วยกับบทความนี้ร่วมกันส่งเสียงให้กับการผูกมัดผู้หญิงด้วยเงินสินสอด
+            </p>
+          </div>
+          
+        </div>
+        <div class="rightcolumn">
+            <div class="card">
+              <h2>ส่งเสียงของคุณให้กับบทความนี้</h2>
+              <tr>
+                <td>
+                    เห็นด้วย
+                    <input type="checkbox" name="เห็นด้วย" id="" value="เห็นด้วย" />
+                    ไม่เห็นด้วย
+                    <input type="checkbox" name="ไม่เห็นด้วย" id="" value="ไม่เห็นด้วย" />
+                    <br />
+                </td>
+            </tr>
+
+            <!----- Submit and Reset ------------------------------------------------->
+            <tr>
+                <td colspan="2" align="center">
+                    <input type="button" value="Submit" onclick="validate_all()">
+                    <input type="reset" value="Reset">
+                </td>
+            </tr>
+              
+            </div>
+        </div> 
+
+    </div>
+        <div class="footer">
+        <br>
+        <p>&copy; 2021 . All rights reserved.</p></br>
+        </div>
+    
+    <!-- Page content -->
+    <div class="content">
+        
+    </div>
+</body>
+</html>
